cebd180d10745e419b9ec30e1a75907e2defee72
antheachan8 <148844539+antheachan8@users.noreply.github.com>
2024-01-08 21:04:39 +0800

    added rest api endpoints and unit of work pattern

--- CarRentalManagement/Shared/Domain/Booking.cs
+++ CarRentalManagement/Shared/Domain/Booking.cs
@@ -4,7 +4,8 @@
     {
         public DateTime DateOut { get; set; }
         public DateTime DateIn { get; set; }
-        public int VehicleId { get; set; }
+        public int ModelId { get; set; }
+        public virtual Model? Model { get; set; }
         public virtual Vehicle? Vehicle { get; set; }
         public int CustomerId { get; set; }
         public virtual Customer? Customer { get; set; }
